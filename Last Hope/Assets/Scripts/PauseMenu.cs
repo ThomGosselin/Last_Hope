de582a56ce75cb1a6e9f26272bb2f2f29c6713cf
ThomGosselin <thomswim360@gmail.com>
2022-01-20 12:50:44 -0500

    Ajout d'un menu pause dans le niveau 1

---
+++ Last Hope/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUi;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadOptions()
+    {
+        Debug.Log("Je suis un beau menu options");
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Retour au menu principale");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Auto destruction dans 5.. 4.. 3.. 2.. 1.. fuck jai pas exploser");
+    }
+}
