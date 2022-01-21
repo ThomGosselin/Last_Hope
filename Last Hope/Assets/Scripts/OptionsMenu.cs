d68f1263495bb1317ece39476069412ef61427b0
ThomGosselin <thomswim360@gmail.com>
2022-01-21 10:26:42 -0500

    Ajout de l'option du choix de la resolutions dans le menu options

--- Last Hope/Assets/Scripts/OptionsMenu.cs
+++ Last Hope/Assets/Scripts/OptionsMenu.cs
@@ -6,9 +6,25 @@ using UnityEngine.UI;
 public class OptionsMenu : MonoBehaviour
 {
     [SerializeField] Slider volumeSlider;
-    // Start is called before the first frame update
+
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
     void Start()
     {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+        }
+
+        resolutionDropdown.AddOptions(options);
+
         if (!PlayerPrefs.HasKey("musicVolume"))
         {
             PlayerPrefs.SetFloat("musicVolume", 1);
