55a585d401b28f442f53ecbd5911267d6ea38465
ThomGosselin <thomswim360@gmail.com>
2022-01-25 14:13:03 -0500

    Ajout systeme d'ouverture et fermeture de l'inventaire

--- Last Hope/Assets/Scripts/InventoryUI.cs
+++ Last Hope/Assets/Scripts/InventoryUI.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public bool isInventoryOpen = false;
+    public GameObject InventoryMenuUi;
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (isInventoryOpen)
+            {
+                CloseInventory();
+            }
+            else
+            {
+                OpenInventory();
+            
+            }
+        }
+    }
+    public void OpenInventory()
+        {
+            InventoryMenuUi.SetActive(true);
+            Time.timeScale = 0f;
+            isInventoryOpen = true;
+        }
+    public void CloseInventory()
+    {
+        InventoryMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        isInventoryOpen = false;
     }
 }
+
+
+
