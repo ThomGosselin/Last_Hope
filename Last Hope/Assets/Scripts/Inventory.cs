c5264bab422b618eef4ffad80ae66a1459fe0eb5
ThomGosselin <thomswim360@gmail.com>
2022-01-26 10:17:24 -0500

    Un objet peux maintenant etre recuperer (et non ajouter dans l'inventaire pour l'instant)

--- Last Hope/Assets/Scripts/Inventory.cs
+++ Last Hope/Assets/Scripts/Inventory.cs
@@ -6,8 +6,28 @@ public class Inventory : MonoBehaviour
 {
     public ArrayList inventory;
 
-    public void fixedUpdate()
-    {
+    public GameObject objets;
+    bool objectToPickup = false;
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("collision");
+        if (collision.CompareTag("Objets")){
+            Debug.Log("C'est un objets");
+            objectToPickup = true;
+
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (objectToPickup == true)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                objets.SetActive(false);
+            }
+        }
     }
 }
---
+++ Last Hope/Assets/Scripts/ObjDesc.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjDesc 
+{
+   public string ObjName;
+   public int ObjQte;
+    public Sprite ObjIcon;
+}
