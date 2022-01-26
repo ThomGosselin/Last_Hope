457964d7f1bc1dc1c8511cb43dfe7d463b4ca48d
ThomGosselin <thomswim360@gmail.com>
2022-01-26 09:09:15 -0500

    Début systeme inventaire

--- Last Hope/Assets/Scripts/GameItem.cs
+++ Last Hope/Assets/Scripts/GameItem.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName ="Inventory Item Data")]
-public class InventoryItemData : ScriptableObject
+public class GameItem : ScriptableObject
 {
     public string id;
     public string displayName;
     public Sprite icon;
-    public GameObject prefab;
+    public Sprite visualObject;
+    public bool pickable;
 }
--- Last Hope/Assets/Scripts/Inventory.cs
+++ Last Hope/Assets/Scripts/Inventory.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public ArrayList inventory;
 
-    // Update is called once per frame
-    void Update()
+    public void fixedUpdate()
     {
-        
+
     }
 }
