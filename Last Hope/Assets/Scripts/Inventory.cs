bd0deffc79d3d056d6c520d036661a17cb936ac0
ThomGosselin <thomswim360@gmail.com>
2022-01-26 10:43:38 -0500

    Progression systeme inventaire

--- Last Hope/Assets/Scripts/Inventory.cs
+++ Last Hope/Assets/Scripts/Inventory.cs
@@ -7,6 +7,7 @@ public class Inventory : MonoBehaviour
     public ArrayList inventory;
 
     public GameObject objets;
+    public ObjDesc description;
     bool objectToPickup = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -27,7 +28,17 @@ public class Inventory : MonoBehaviour
             if (Input.GetKey(KeyCode.E))
             {
                 objets.SetActive(false);
+                addToInventory();
             }
+        }
+    }
+
+    public void addToInventory()
+    {
+        if (inventory.Count != 7) //A CHANGER LORSQUE LE NOMBRE FINAL SERA DETERMINER
+        {
+            inventory.Add(objets);
+            Debug.Log(inventory);
         }
     }
 }
--- Last Hope/Assets/Scripts/ObjDesc.cs
+++ Last Hope/Assets/Scripts/ObjDesc.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjDesc 
+public class ObjDesc : MonoBehaviour
 {
    public string ObjName;
    public int ObjQte;
-    public Sprite ObjIcon;
+   public Sprite ObjIcon;
 }
