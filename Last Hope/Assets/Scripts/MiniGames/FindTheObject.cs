ac0e631b51b7ae51efc7e044d65204ef7f97cf85
ThomGosselin <thomswim360@gmail.com>
2022-01-27 13:21:55 -0500

    Quelques sont ont été modifier + progression Find The Object MiniGame

--- Last Hope/Assets/Scripts/MiniGames/FindTheObject.cs
+++ Last Hope/Assets/Scripts/MiniGames/FindTheObject.cs
@@ -1,11 +1,21 @@
     using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FindTheObject : MonoBehaviour
 {
+
+    public Sprite imgToFind;
     private void FixedUpdate()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Click");
+            if (imgToFind.CompareTag("ItemToFind"))
+            {
+                Debug.Log("Bingo!");
+            }
+        }
     }
 }
