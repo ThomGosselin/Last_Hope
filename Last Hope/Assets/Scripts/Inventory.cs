865fc196a83a5c61dc534ba92113475329e7920e
ThomGosselin <thomswim360@gmail.com>
2022-01-27 09:10:53 -0500

    Retour en arriere systeme inventaire

--- Last Hope/Assets/Scripts/Inventory.cs
+++ Last Hope/Assets/Scripts/Inventory.cs
@@ -5,35 +5,30 @@ using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    
-    public  List<ObjDesc> inventory;
-    public  Image[] inventorySpaces;
+    public List<ObjDesc> inventory;
+    public Image[] inventorySpaces;
 
-    public GameObject[] pickableItems;
-    public List<GameObject> objets;
+    public GameObject objets;
     public GameObject invUI;
     public ObjDesc description;
-    public GameObject Obj1;
-    public GameObject Obj2;
-    public GameObject Obj3;
-    public GameObject Obj4;
-    public GameObject Obj5;
     bool objectToPickup = false;
     int invIndex = 0;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("collision");
-        if (collision.CompareTag("Objets")){
+        if (collision.CompareTag("Objets"))
+        {
             Debug.Log("C'est un objets");
             objectToPickup = true;
+
+
         }
     }
 
     private void Start()
     {
         inventorySpaces = invUI.GetComponentsInChildren<Image>();
-        Debug.Log(pickableItems);
     }
 
     private void FixedUpdate()
@@ -42,25 +37,9 @@ public class Inventory : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                Debug.Log(description.ObjIndex);
-                DetItem();
+                objets.SetActive(false);
+                addToInventory();
             }
-        }
-    }
-
-    public void DetItem()
-    {
-        switch (description.ObjIndex)
-        {
-            case 1:
-                Debug.Log("1");
-                objets.Add(Obj1);
-                Obj1.SetActive(false);
-                break;
-            case 2:
-                objets.Add(Obj2);
-                Obj2.SetActive(false);
-                break;
         }
     }
 
