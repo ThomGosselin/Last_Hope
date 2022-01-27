0fddbf8f7f870c393a3b8b659407b699c35a8168
ThomGosselin <thomswim360@gmail.com>
2022-01-26 21:00:17 -0500

    debut nouveau systeme objets pour l'inventaire

--- Last Hope/Assets/Scripts/Inventory.cs
+++ Last Hope/Assets/Scripts/Inventory.cs
@@ -5,12 +5,19 @@ using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
+    
     public  List<ObjDesc> inventory;
     public  Image[] inventorySpaces;
 
-    public GameObject objets;
+    public GameObject[] pickableItems;
+    public List<GameObject> objets;
     public GameObject invUI;
     public ObjDesc description;
+    public GameObject Obj1;
+    public GameObject Obj2;
+    public GameObject Obj3;
+    public GameObject Obj4;
+    public GameObject Obj5;
     bool objectToPickup = false;
     int invIndex = 0;
 
@@ -20,14 +27,13 @@ public class Inventory : MonoBehaviour
         if (collision.CompareTag("Objets")){
             Debug.Log("C'est un objets");
             objectToPickup = true;
-
-
         }
     }
 
     private void Start()
     {
-        inventorySpaces = invUI.GetComponentsInChildren<Image>();   
+        inventorySpaces = invUI.GetComponentsInChildren<Image>();
+        Debug.Log(pickableItems);
     }
 
     private void FixedUpdate()
@@ -36,9 +42,25 @@ public class Inventory : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                objets.SetActive(false);
-                addToInventory();
+                Debug.Log(description.ObjIndex);
+                DetItem();
             }
+        }
+    }
+
+    public void DetItem()
+    {
+        switch (description.ObjIndex)
+        {
+            case 1:
+                Debug.Log("1");
+                objets.Add(Obj1);
+                Obj1.SetActive(false);
+                break;
+            case 2:
+                objets.Add(Obj2);
+                Obj2.SetActive(false);
+                break;
         }
     }
 
