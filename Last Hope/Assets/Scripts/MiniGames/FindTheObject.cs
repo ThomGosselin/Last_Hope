e3f2a950c1b2e3bf2769c4116f34d3539e656f7a
ThomGosselin <thomswim360@gmail.com>
2022-01-27 12:50:27 -0500

    Ajout de différents éléments

---
+++ Last Hope/Assets/Scripts/MiniGames/FindTheObject.cs
@@ -0,0 +1,11 @@
+    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindTheObject : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        
+    }
+}
