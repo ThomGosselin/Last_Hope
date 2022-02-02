61896d3841048bd4294f922ee70d272af345c55e
ThomGosselin <thomswim360@gmail.com>
2022-02-02 12:42:41 -0500

    Minigame 2 ajout start of game + progression

--- Last Hope/Assets/Scripts/MiniGames/unlockVault.cs
+++ Last Hope/Assets/Scripts/MiniGames/unlockVault.cs
@@ -8,33 +8,55 @@ public class unlockVault : MonoBehaviour
     public GameObject[] circleTab;
     public GameObject[] selectedCircleTab;
     public GameObject gameGrid;
-    public float randomCircle;
+    public int randomCircle;
     public int circleToRemember;
     public int i;
     public int oldNumber;
     public GameObject selectedCircle;
     public GameObject correctCircle;
     public int lifes;
-
+    public int numberRange;
 
     void Start()
     {
+        numberRange = circleTab.Length;
         GenerateRandomCircles();
-        StartCoroutine(waitForStartGames());
+    
+            StartCoroutine(waitForStartGames());
     }
 
     IEnumerator waitForStartGames()
     {
         yield return new WaitForSeconds(3);
-        Debug.Log("start");
+        for (i = 0; i < circleTab.Length; i++)
+        {
+            circleTab[i].gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+
+        }
+            Debug.Log("start");
     }
 
 
     private void GenerateRandomCircles()
     {
-
+        //generation d'un cercle random dans la grille de jeu
+        for (i = 0; i < circleToRemember; i++)
+        {
+            randomCircle = Random.Range(1, circleTab.Length - 1);
+            if (randomCircle == oldNumber)
+            {
+                i--;
+            }
+            else
+            {
+                Debug.Log(randomCircle);
+                circleTab[randomCircle - 1].transform.gameObject.tag = "selectedCricles";
+                circleTab[randomCircle - 1].gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+                oldNumber = randomCircle;
+            }
+           
+        }
     }
-
 
     public void Update()
     {
@@ -48,14 +70,19 @@ public class unlockVault : MonoBehaviour
                 RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
                 if (hit.collider.CompareTag("selectedCricles"))
                 {
-                    FindObjectOfType<AudioManager>().Play("SuccedMiniGame");
+                    FindObjectOfType<AudioManager>().Play("goodChoice");
                     hit.collider.gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+                    circleTab[randomCircle - 1].transform.gameObject.tag = "selected";
                 }
                 else if (hit.collider.CompareTag("circles"))
                 {
-                    FindObjectOfType<AudioManager>().Play("InvFull");
+                    FindObjectOfType<AudioManager>().Play("badChoice");
                     hit.collider.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
                     lifes = lifes - 1;
+                }
+                else if (!hit)
+                {
+                    return;
                 }
             }
             else
@@ -63,8 +90,9 @@ public class unlockVault : MonoBehaviour
                 gameGrid.SetActive(false);
                 FindObjectOfType<AudioManager>().Play("ErrorMiniGame");
             }
-           
+        //code verification si le joueur a tout trouver 
         }
+        
     }
 
 }
