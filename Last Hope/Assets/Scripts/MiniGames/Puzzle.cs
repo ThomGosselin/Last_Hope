cbba7e739969798610318789f21c7c5640dcb4ee
ThomGosselin <thomswim360@gmail.com>
2022-02-10 09:01:30 -0500

    Ajout mini jeu 3 (puzzle) lvl2

--- Last Hope/Assets/Scripts/MiniGames/Puzzle.cs
+++ Last Hope/Assets/Scripts/MiniGames/Puzzle.cs
@@ -14,7 +14,11 @@ public class Puzzle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Init(); 
+        Init();
+        for (int i = 0;i< 999; i++)
+        {
+            Shuffle();
+        }
     }
 
     void Init()
@@ -24,11 +28,114 @@ public class Puzzle : MonoBehaviour
         for(int x = 0; x < 4; x++)
             {
                 numberBox box = Instantiate(boxPrefab, new Vector2(x, y), Quaternion.identity);
-                box.Init(x, y, n + 1, sprites[n]);
+                box.Init(x, y, n + 1, sprites[n], CLickToSwap );
                 boxes[x, y] = box;
                 n++;
             }
+    }
+    void CLickToSwap(int x,int y)
+    {
+        int Dx = getDx(x, y);
+        int Dy = getDy(x, y);
+        Swap(x, y, Dx, Dy);
+       
+    }
 
+    void Swap(int x, int y, int Dx, int Dy)
+    {
+        var from = boxes[x, y];
+        var target = boxes[x + Dx, y + Dy];
 
+        //swap this 2 boxes
+        boxes[x, y] = target;
+        boxes[x + Dx, y + Dy] = from;
+
+        //update pos 2 boxes;
+        from.UpdatePos(x + Dx, y + Dy);
+        target.UpdatePos(x, y);
+    }
+
+    int getDx(int x, int y)
+    {
+        //is right empty
+        if (x < 3 && boxes[x + 1, y].isEmpty())
+        {
+            return 1;
+        }
+        //is left empty
+        else if (x > 0 && boxes[x - 1, y].isEmpty())
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+        
+    }
+
+    int getDy(int x, int y)
+    {
+        //is top empty
+        if (y < 3 && boxes[x, y + 1].isEmpty())
+        {
+            return 1;
+        }
+        //is bottom empty
+        else if (y > 0 && boxes[x, y - 1].isEmpty())
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    void Shuffle()
+    {
+        for(int i =0; i<4; i++)
+        {
+            for (int j =0; j<4; j++)
+            {
+                if (boxes[i, j].isEmpty())
+                {
+                    Vector2 pos = getValidMove(i, j);
+                    Swap(i, j, (int)pos.x, (int)pos.y);
+                }
+            }
+        }
+    }
+
+    private Vector2 lastMove;
+
+    Vector2 getValidMove(int x, int y)
+    {
+        Vector2 pos = new Vector2();
+        do
+        {
+            int n = Random.Range(0, 4);
+            if (n == 0)
+                pos = Vector2.left;
+            else if (n == 1)
+                pos = Vector2.right;
+            else if (n == 2)
+                pos = Vector2.up;
+            else
+                pos = Vector2.down;
+        } while (!(isValidRange(x + (int)pos.x) && isValidRange(y + (int)pos.y)) || isRepeatMove(pos));
+
+        lastMove = pos;
+        return pos;
+    }
+
+    bool isValidRange(int n)
+    {
+        return n >= 0 && n <= 3;
+    }
+
+    bool isRepeatMove(Vector2 pos)
+    {
+        return pos * -1 == lastMove;
     }
 }
--- Last Hope/Assets/Scripts/MiniGames/numberBox.cs
+++ Last Hope/Assets/Scripts/MiniGames/numberBox.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,17 +9,50 @@ public class numberBox : MonoBehaviour
     int x = 0;
     int y = 0;
 
-    public void Init(int i, int j, int index, Sprite sprite)
+    private Action<int, int> swapFunc = null;
+
+    public void Init(int i, int j, int index, Sprite sprite, Action<int, int> swapFunc)
     {
         this.index = index;
         this.GetComponent<SpriteRenderer>().sprite = sprite;
         UpdatePos(i, j);
+        this.swapFunc = swapFunc;
     }
 
     public void UpdatePos(int i, int j)
     {
         x = i;
         y = j;
-        this.gameObject.transform.localPosition = new Vector2(i, j);
+        StartCoroutine(Move());
+    }
+
+    IEnumerator Move()
+    {
+        float elapsedTime = 0;
+        float duration = 0.1f;
+        Vector2 start = this.gameObject.transform.localPosition;
+        Vector2 end = new Vector2(x, y);
+
+        while (elapsedTime < duration)
+        {
+            this.gameObject.transform.localPosition = Vector2.Lerp(start, end, (elapsedTime / duration));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        this.gameObject.transform.localPosition = end;
+    }
+
+    public bool isEmpty()
+    {
+        return index == 16;
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0) && swapFunc != null)
+        {
+            swapFunc(x, y);
+        }
     }
 }
