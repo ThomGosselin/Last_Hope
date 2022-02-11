5c2bc15f4a8d3ce5697499d8bb7f9f6cfc1f00d4
ThomGosselin <thomswim360@gmail.com>
2022-02-11 13:50:11 -0500

    Ajout de la possibilité d'ouvrir l'applicaiton word sur l'ecran de l'ordinateur pour l'enigme 2 level 2

--- Last Hope/Assets/Scripts/MiniGames/OpenWord.cs
+++ Last Hope/Assets/Scripts/MiniGames/OpenWord.cs
@@ -1,9 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpenWord : MonoBehaviour
 {
     public GameObject currentComputerScreen;
     public GameObject wordApplication;
+
+    
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            if (hit.collider.CompareTag("icnWord"))
+            {
+                currentComputerScreen.SetActive(false);
+                wordApplication.SetActive(true);
+            }
+        }
+    }
 }
