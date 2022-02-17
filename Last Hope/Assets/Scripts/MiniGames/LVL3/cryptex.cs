e36d07556f47bc406f1a0332d5c2a91a529ea067
ThomGosselin <thomswim360@gmail.com>
2022-02-17 12:59:54 -0500

    Debut de la programmation du jeu avec le cryptex

---
+++ Last Hope/Assets/Scripts/MiniGames/LVL3/cryptex.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cryptex : MonoBehaviour
+{
+    [Header("Input Field")]
+    public InputField Letter1;
+    public InputField Letter2;
+    public InputField Letter3;
+    public InputField Letter4;
+}
