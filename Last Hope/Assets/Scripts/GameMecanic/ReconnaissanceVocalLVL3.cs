11afc70589f45a69a0b457f3f7f8cd0b7f699a27
ThomGosselin <thomswim360@gmail.com>
2022-02-18 11:24:48 -0500

    Progression de la programmation de la reconnaissance vocal de la 3e enigme du niveau 3

--- Last Hope/Assets/Scripts/GameMecanic/ReconnaissanceVocalLVL3.cs
+++ Last Hope/Assets/Scripts/GameMecanic/ReconnaissanceVocalLVL3.cs
@@ -7,17 +7,24 @@ using System.Linq;
 
 public class ReconnaissanceVocalLVL3 : MonoBehaviour
 {
-
+    [Header("Éléments pour la reconnaissance vocal")]
     private Dictionary<string, Action> keywordActions = new Dictionary<string, Action>();
     private KeywordRecognizer keywordRecognizer;
+    [Header("Éléments de l'enigme 3")]
+    public string VoiceMailPassword;
+    public string UserVoiceMailPassWord;
+    public int CurrentPasswordLenght;
+    public int lenghtOfCurrentTry;
     void Start()
     {
+        CurrentPasswordLenght = VoiceMailPassword.Length;
+        //Ajout des mots clefs pour la reconnaissance vocal
         keywordActions.Add("dernier espoir", OpenDoorLvl3);
         keywordActions.Add("last hope", OpenDoorLvl3);
-        keywordActions.Add("7", OpenDoorLvl3);
-        keywordActions.Add("5", OpenDoorLvl3);
-        keywordActions.Add("8", OpenDoorLvl3);
-        keywordActions.Add("1", OpenDoorLvl3);
+        keywordActions.Add("7", AddToVoiceMailPassword7);
+        keywordActions.Add("5", AddToVoiceMailPassword5);
+        keywordActions.Add("8", AddToVoiceMailPassword8);
+        keywordActions.Add("1", AddToVoiceMailPassword1);
 
         keywordRecognizer = new KeywordRecognizer(keywordActions.Keys.ToArray());
         keywordRecognizer.OnPhraseRecognized += OnKeywordsRecognized;
@@ -34,7 +41,64 @@ public class ReconnaissanceVocalLVL3 : MonoBehaviour
 
     private void OpenDoorLvl3()
     {
-        
+        //code de deplacement de la porte
     }
 
+    private void AddToVoiceMailPassword7()
+    {
+        UserVoiceMailPassWord = UserVoiceMailPassWord + "7";
+        if (lenghtOfCurrentTry == CurrentPasswordLenght)
+        {
+            checkPassWord();
+        }
+        else
+        {
+            lenghtOfCurrentTry = lenghtOfCurrentTry + 1;  
+        }
+       
+    }
+    private void AddToVoiceMailPassword5()
+    {
+        UserVoiceMailPassWord = UserVoiceMailPassWord + "5";
+        if (lenghtOfCurrentTry == CurrentPasswordLenght)
+        {
+            checkPassWord();
+        }
+        else
+        {
+            lenghtOfCurrentTry = lenghtOfCurrentTry + 1;
+        }
+    }
+    private void AddToVoiceMailPassword8()
+    {
+        UserVoiceMailPassWord = UserVoiceMailPassWord + "8";
+        if (lenghtOfCurrentTry == CurrentPasswordLenght)
+        {
+            checkPassWord();
+        }
+        else
+        {
+            lenghtOfCurrentTry = lenghtOfCurrentTry + 1;
+        }
+    }
+    private void AddToVoiceMailPassword1()
+    {
+        UserVoiceMailPassWord = UserVoiceMailPassWord + "1";
+        if (lenghtOfCurrentTry == CurrentPasswordLenght)
+        {
+            checkPassWord();
+        }
+        else
+        {
+            lenghtOfCurrentTry = lenghtOfCurrentTry + 1;
+        }
+    }
+
+    private void checkPassWord()
+    {
+        if (UserVoiceMailPassWord == VoiceMailPassword)
+        {
+            //code pour ce la suite de l'enigme
+        }
+    }
 }
