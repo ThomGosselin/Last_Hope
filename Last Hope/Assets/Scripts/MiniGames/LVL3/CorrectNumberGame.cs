12210e51d050ec15360667548adadd6db6feb4cf
ThomGosselin <thomswim360@gmail.com>
2022-02-18 09:36:34 -0500

    Ajout Code Enigme 2 level 3

--- Last Hope/Assets/Scripts/MiniGames/LVL3/CorrectNumberGame.cs
+++ Last Hope/Assets/Scripts/MiniGames/LVL3/CorrectNumberGame.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CorrectNumberGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Input Field")]
+    public InputField NumberInputFromUser;
+    [Header("Number To Find")]
+    public string NumberToFind;
+    [Header("Closing Game Section")]
+    public GameObject GameGrid;
+    public GameObject Player;
+    public GameObject xBtn;
+
+
+    private void Update()
     {
-        
+        if (NumberInputFromUser.text == NumberToFind)
+        {
+            FindObjectOfType<AudioManager>().Play("SuccedMiniGame");
+            GameGrid.SetActive(false);
+            Player.SetActive(true);
+            xBtn.SetActive(true);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
