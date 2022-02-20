184180d0bd66e450567a51eaaa9b5dbf697114cb
ThomGosselin <thomswim360@gmail.com>
2022-02-20 16:33:20 -0500

    Ajout fonctionnalité de item glowing +  ajout élément visuel enigme 3

---
+++ Last Hope/Assets/Scripts/GameMecanic/GlowObject.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowObject : MonoBehaviour
+{
+    private SpriteRenderer _renderer;
+    private bool _isGlowing = false;
+    private bool _glowUp = true;
+    private float _blinkTimer = 0.5f;
+    private float _glowValue = 1f;
+
+    void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+
+
+        //Ajuste l'intensité du glow de matériel en clignotement
+        if (_isGlowing)
+        {
+            _blinkTimer -= Time.deltaTime;
+            if (_blinkTimer < 0f)
+            {
+                _blinkTimer = 0.5f;
+                _glowUp = _glowUp ? false : true;
+            }
+
+            if (_glowUp)
+            {
+                _glowValue = 2f - _blinkTimer * 2f;
+            }
+            else
+            {
+                _glowValue = 1f + _blinkTimer * 2f;
+            }
+        }
+
+        else
+        {
+            _glowValue -= Time.deltaTime;
+
+            if (_glowValue < 1f)
+            {
+                _glowValue = 1f;
+            }
+        }
+
+        _renderer.material.SetFloat("_intensity", _glowValue);
+
+    }
+
+    private void OnTriggerEnter2D()
+    {
+        _isGlowing = true;
+    }
+
+    private void OnTriggerExit2D()
+    {
+        _isGlowing = false;
+    }
+}
