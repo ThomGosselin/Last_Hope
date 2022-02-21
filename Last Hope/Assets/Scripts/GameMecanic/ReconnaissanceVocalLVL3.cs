ba6b82a64db18d3eb276a9d70d32a3a5be3221a5
ThomGosselin <thomswim360@gmail.com>
2022-02-21 11:47:06 -0500

    Retrait d'un debug.log pour garder la console clean

--- Last Hope/Assets/Scripts/GameMecanic/ReconnaissanceVocalLVL3.cs
+++ Last Hope/Assets/Scripts/GameMecanic/ReconnaissanceVocalLVL3.cs
@@ -36,7 +36,6 @@ public class ReconnaissanceVocalLVL3 : MonoBehaviour
 
     private void OnKeywordsRecognized(PhraseRecognizedEventArgs args)
     {
-        Debug.Log("Test");
         keywordActions[args.text].Invoke();
     }
 
